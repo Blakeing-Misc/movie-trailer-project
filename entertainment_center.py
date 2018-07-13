8156f081dd29e8f07fe71e6fbe78f227452cea2b
Blakeing <blakeingenthron@gmail.com>
2018-07-13 18:06:33 -0500

    Initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,31 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://image.tmdb.org/t/p/w600_and_h900_bestv2/rhIRbceoE9lR4veEXuwCC2wARtG.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://resizing.flixster.com/0kS0YraghPsLFqJi2sPCxYwqAeg=/206x305/v1.bTsxMTE3Njc5MjtqOzE3NzI0OzEyMDA7ODAwOzEyMDA",
+                     "https://www.youtube.com/watch?v=-9ceBgWV8io")
+inception = media.Movie("Inception",
+                        "A thief, who steals corporate secrets through the use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+                        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                        "https://www.youtube.com/watch?v=YoHD9XEInc0")
+the_dark_knight = media.Movie("The Dark Knight",
+                              "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+                              "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                              "https://www.youtube.com/watch?v=EXeTwQWrcwY")
+interstellar = media.Movie("Interstellar",
+                           "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+                           "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                           "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
+the_godfather = media.Movie("The Godfather",
+                            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+                            "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
+                            "https://www.youtube.com/watch?v=sY1S34973zA")
+
+movies = [toy_story, avatar, inception,
+          the_dark_knight, interstellar, the_godfather]
+fresh_tomatoes.open_movies_page(movies)
